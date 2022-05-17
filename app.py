ed03cc2695b207759c29a860503bd6d02d5c2534
maimedlord <52831314+maimedlord@users.noreply.github.com>
2022-05-17 13:05:53 -0400

    remove logged_in.html

--- app.py
+++ app.py
@@ -11,31 +11,37 @@ login_mgr = LoginManager()
 
 @app.route('/', methods=['post', 'get'])
 def index():
+    message = ''
     if "email" in session:
-        return redirect(url_for('logged_in'))
+        return redirect(url_for('account.html'))
     if request.method == 'POST':
-        user = request.form.get('fullname')
         email = request.form.get('email')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-        user_found = request.form.get('...')
-        email_found = request.form.get('.....')
-        if user_found:
-            message = 'There is already a user by that name'
-            return render_template('index.html', message=message)
-        if email_found:
-            message = 'This email already exists.'
-            return render_template('index.html', message=message)
-        if password1 != password2:
-            message = 'incorrect password'
-            return render_template('index.html', message=message)
-        else:
-            user_input = {'name': user, 'email': email, 'password': password2}
-            #records.insert_one(user_input) # records = database collection
-            #user_data = records.find_one({'email': email})
-            #new_email = user_data['email']
-            #return render_template('logged_in.html', email=new_email)
-            pass
+        pass_word = request.form.get('pass_word')
+        #user_found =
+
+    # if request.method == 'POST':
+    #     user = request.form.get('fullname')
+    #     email = request.form.get('email')
+    #     password1 = request.form.get('password1')
+    #     password2 = request.form.get('password2')
+    #     user_found = request.form.get('...')
+    #     email_found = request.form.get('.....')
+    #     if user_found:
+    #         message = 'There is already a user by that name'
+    #         return render_template('index.html', message=message)
+    #     if email_found:
+    #         message = 'This email already exists.'
+    #         return render_template('index.html', message=message)
+    #     if password1 != password2:
+    #         message = 'incorrect password'
+    #         return render_template('index.html', message=message)
+    #     else:
+    #         user_input = {'name': user, 'email': email, 'password': password2}
+    #         records.insert_one(user_input) # records = database collection
+    #         user_data = records.find_one({'email': email})
+    #         new_email = user_data['email']
+    #         return render_template('account.html', email=new_email)
+    #         pass
 
     temp_array = processing.get_orders_top()
     return render_template('index.html', temp_array=temp_array)
