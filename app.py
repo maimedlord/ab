aae8ed6a813a60323eb4d1073652c4b76a26fb8c
maimedlord <52831314+maimedlord@users.noreply.github.com>
2022-05-24 23:11:15 -0400

    committing just to be safe...

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Flask, redirect, render_template, request, session, url_for, flash
+from flask import Flask, redirect, render_template, request, session, url_for, flash, Markup
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 import processing as prc
 import calls
+# import html
 from user import User
 
 
@@ -34,6 +35,7 @@ INCOMPLETE
 '''
 @app.route('/', methods=['post', 'get'])
 def index():
+    ip_address = request.remote_addr
     temp_array = prc.get_orders_top()
     if request.method == 'POST':
         email = request.form.get('user_email')
@@ -48,9 +50,9 @@ def index():
             return render_template('account.html')
         else:
             print("failed login")
-            return render_template('index.html', temp_array=temp_array)
+            return render_template('index.html', ip_address=ip_address, temp_array=temp_array)
     else:
-        return render_template('index.html', temp_array=temp_array)
+        return render_template('index.html', ip_address=ip_address, temp_array=temp_array)
 
 
 '''
@@ -59,8 +61,31 @@ INCOMPLETE
 @app.route('/account', methods=['post', 'get'])
 @login_required
 def account():
+    ip_address = request.remote_addr
+    return render_template('account.html', ip_address=ip_address)
 
-    return render_template('account.html')
+
+'''
+'''
+@app.route('/login', methods=['post', 'get'])
+def login():
+    ip_address = request.remote_addr
+    temp_array = prc.get_orders_top()
+    if request.method == 'POST':
+        email = request.form.get('user_email')
+        password = request.form.get('user_password')
+        user_obj = calls.is_auth_user(email, password)
+        if user_obj:
+            user_obj = User(email, user_obj['uName'])
+            login_user(user_obj)
+            # next = flask.request.args.get('next')
+            # if not is_safe_url(next):
+            #     return flask.abort(400)
+            return redirect('account')
+        else:
+            print("failed login")
+            return render_template('index.html', ip_address=ip_address, temp_array=temp_array)
+    return redirect('/')
 
 
 '''
@@ -68,6 +93,7 @@ INCOMPLETE
 '''
 @app.route('/logout', methods=['post', 'get'])
 def logout():
+    ip_address = request.remote_addr
     message = ''
     if not current_user.is_authenticated:
         message = 'You\'re already logged out...'
@@ -82,12 +108,22 @@ def logout():
             # next = flask.request.args.get('next')
             # if not is_safe_url(next):
             #     return flask.abort(400)
-            return render_template('account.html')
+            return render_template('account.html', ip_address=ip_address)
         else:
             print("failed login")
-            return render_template('index.html')
+            return render_template('index.html', ip_address=ip_address)
     else:
-        return render_template('logout.html', message=message)
+        return render_template('logout.html', ip_address=ip_address, message=message)
+
+
+'''
+INCOMPLETE
+'''
+@app.route('/market', methods=['post', 'get'])
+@login_required
+def market():
+    ip_address = request.remote_addr
+    return render_template('market.html', ip_address=ip_address)
 
 
 '''
@@ -95,15 +131,20 @@ INCOMPLETE
 '''
 @app.route('/register', methods=['post', 'get'])
 def register():
+    ip_address = request.remote_addr
+    message = ''
     temp_array = prc.get_orders_top()
     if current_user.is_authenticated:
-        return render_template('index.html', temp_array=temp_array)
+        logout_user()
+        return render_template('hmm.html', ip_address=ip_address, message='You\'re already registered...')
     else:
         if request.method == 'POST':
             email = request.form.get('user_email')
             password1 = request.form.get('user_password1')
             password2 = request.form.get('user_password2')
             username = request.form.get('username')
+            if email == '' or password1 == '' or password2 == '' or username == '':
+                return render_template('register.html', ip_address=ip_address, temp_array=temp_array)
             dict_template = {
                 'active': True,
                 'email': email,
@@ -123,15 +164,7 @@ def register():
             else:
                 return render_template('register.html', message='email or username is already taken. try again')
 
-        return render_template('register.html', temp_array=temp_array)
-
-
-'''
-INCOMPLETE
-'''
-@app.route('/market')
-def market():
-    return render_template('market.html')
+        return render_template('register.html', ip_address=ip_address, temp_array=temp_array)
 
 
 '''
