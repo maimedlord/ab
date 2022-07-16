9dedd0a09f21a04fb232316a89a9d0e9069674cb
maimedlord <52831314+maimedlord@users.noreply.github.com>
2022-07-16 02:59:23 -0400

    good javascript progress account

--- static/scripts/account.js
+++ static/scripts/account.js
@@ -1,83 +1,103 @@
+// values in table columns mapped to array values:
+COLUMN_TO_NUM_ARR = ['position', 'phase', 'type', 'lostudy', 'subject', 'specialization', 'deadline1', 'deadline2', 'bounty', 'efbonus', 'egbonus', 'newchatmsg']
 // grab rows in table and feed them into array.
 // resort array by user-requested value
 // repost data from new array
-function sort_alpha_ascending(sortby) {
-    // var div_obj = document.getElementById('container_contracts_table').children;
-    //
-    // if (div_obj.length < 2) {
-    //     console.log('less than two contracts...');
-    //     return;
-    // }
-    // for (var i = 0; i < div_obj.length; i++) {
-    //     console.log(div_obj[i].innerHTML)
-    // }
-    //
-    //console.log(Object.values(div_obj)[0]);//highlightable in console to dom - each row surrounded by <a>
-    console.log('here');
-    //console.log($('#container_contracts_table')[0].children);
-    //some_obj = $('#container_contracts_table')[0].children.sort();
-    table_only = $('#container_contracts_table');
-
-
+function sort_alpha(direction, sortby) {
+    // sorting function:
+    var fn;
+    if (direction == 'up') {
+        fn = function (a, b) {
+            console.log(('' + a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString()).localeCompare(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString()));
+            return ('' + a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString()).localeCompare(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString());
+        }
+    }
+    else {
+        fn = function (a, b) {
+            console.log(a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString());
+            console.log(-1 * ('' + a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString()).localeCompare(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString()));
+            return -1 * ('' + a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString()).localeCompare(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML.toString());
+        }
+    }
+    table_div = $('#container_contracts_table');
     table_children = $('#container_contracts_table')[0].children;
+    // transfer to array so can sort:
     some_arr = [];
     for (var i = 0; i < table_children.length; i++) {
-        //console.log(table_children.item(i));
         some_arr.push(table_children.item(i));
     }
-    console.log('zzzzzzzzzz');
+    // sort the array alphabetically:
+    some_arr.sort(fn);
+    // publish changes to DOM:
     some_arr.forEach(element => {
-        // console.log(element);
-        // console.log(element.children[0].children[0].innerHTML);
-    });
-    some_arr.sort(function (a, b) {
-        return ('' + a.children[0].children[0].innerHTML.toString()).localeCompare(b.children[0].children[0].innerHTML.toString());
+        table_div.append(element)
     })
-    for (var i = 0; i < some_arr.length; i++) {
-        console.log(some_arr[i]);
-    }
-    for (var i = 0; i < some_arr.length; i++) {
-        table_only.append(some_arr[i]);
-    }
+}
 
-
-    //table_only.prepend("<div>yoyo</div>")
-
-
-
-    ///////////////////////////////
-    // html_collection = $('#container_contracts_table')[0].children;
-    // console.log(html_collection);
-    // for (var i = 0; i < html_collection.length; i++) {
-    //     //var string1 = html_collection.item(i).children[0].children[0].innerHTML;
-    //     //console.log(html_collection.item(i).children[0].children[0].innerHTML);
-    //     console.log(string1.toString());
-    // }
-
-
+function sort_date(direction, sortby) {
+    // sorting function:
     var fn;
-    fn = function(a, b) {return(a.price - b.price);}
-    // if (direction == "down") {
-    //     fn = function(a, b) {return(a.price - b.price);}
-    // } else {
-    //     fn = function(a, b) {return(b.price - a.price);}
-    // }
-    // html_collection = $('#container_contracts_table')[0].children;
-    // for (var i = 0; i < html_collection.length; i++) {
-    //     console.log(html_collection.item(i));
-    // }
-
-    console.log('here');
-    //var arr = [];
-    // for (element in div_arr) {
-    //     console.log(element);
-    // }
+    if (direction == 'up') {
+        fn = function (a, b) {
+            var a_result = new Date(a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML).toISOString();
+            var b_result = new Date(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML).toISOString();
+            if (a_result > b_result) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+    else {
+        fn = function (a, b) {
+            var a_result = new Date(a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML).toISOString();
+            var b_result = new Date(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML).toISOString();
+            if (a_result > b_result) {
+                return -1;
+            }
+            else {
+                return 1;
+            }
+        }
+    }
+    table_div = $('#container_contracts_table');
+    table_children = $('#container_contracts_table')[0].children;
+    // transfer to array so can sort:
+    some_arr = [];
+    for (var i = 0; i < table_children.length; i++) {
+        some_arr.push(table_children.item(i));
+    }
+    some_arr.sort(fn);
+    // publish changes to DOM:
+    some_arr.forEach(element => {
+        table_div.append(element)
+    })
 }
-function sort_alpha_descending(sortby) {
-    window.alert('up');
-}
 
-//
-function sort_alpha(direction, sortby) {
-
+function sort_num(direction, sortby) {
+    // sorting function:
+    var fn;
+    if (direction == 'up') {
+        fn = function (a, b) {
+            return parseFloat(a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML) - parseFloat(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML);
+        }
+    }
+    else {
+        fn = function (a, b) {
+            return parseFloat(b.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML) - parseFloat(a.children[0].children[COLUMN_TO_NUM_ARR.indexOf(sortby)].innerHTML);
+        }
+    }
+    table_div = $('#container_contracts_table');
+    table_children = $('#container_contracts_table')[0].children;
+    // transfer to array so can sort:
+    some_arr = [];
+    for (var i = 0; i < table_children.length; i++) {
+        some_arr.push(table_children.item(i));
+    }
+    some_arr.sort(fn);
+    // publish changes to DOM:
+    some_arr.forEach(element => {
+        table_div.append(element)
+    })
 }
